b9742951d6512b93f4bf5625cd362be4f6daf9ea
BoomChukkaLaka <boomchukkalaka@gmail.com>
2024-03-13 15:57:41 +1000

    Initial commit - Week 4 - Infiltration Software Iteration 4 Completed

--- src/example.java
+++ src/example.java
@@ -59,7 +59,8 @@ class Main {
      */
     public static ArrayList<Obstacle> parseObstacles(HashMap<String, ArrayList<String>> parsedArgs) {
         ArrayList<Obstacle> obstacles = new ArrayList<>();
-        ObstacleType type = ObstacleType.GUARD;
+        /** REMOVED and replaced with updated code.
+         ObstacleType type = ObstacleType.GUARD;
         String key = "-" + type.getArgumentName();
         ArrayList<String> args = parsedArgs.get(key);
         if (args == null) {
@@ -70,6 +71,25 @@ class Main {
             String cleanedArg = stripParentheses(arg);
             Obstacle obstacle = Guard.parse(cleanedArg);
             obstacles.add(obstacle);
+        }
+         **/
+        for (ObstacleType type : ObstacleType.values()) {
+            String key = "-" + type.getArgumentName();
+            ArrayList<String> args = parsedArgs.get(key);
+            if (args == null) {
+                continue;
+            }
+            for (String arg : args) {
+                // Remove the parentheses from the argument
+                String cleanedArg = stripParentheses(arg);
+                Obstacle obstacle = switch (type) {
+                    case GUARD -> Guard.parse(cleanedArg);
+                    case FENCE -> Fence.parse(cleanedArg);
+                    case SENSOR -> Sensor.parse(cleanedArg);
+                    case CAMERA -> Camera.parse(cleanedArg);
+                };
+                obstacles.add(obstacle);
+            }
         }
         return obstacles;
     }
